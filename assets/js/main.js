7c420981a00fc3da578782332fd6bd5248694c8a
JordanMalekeraVives <jordan.malekera@student.vives.be>
2022-10-07 00:07:32 +0200

    Design overhaul

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+//Preloader
+const load = document.querySelector(".preloader");
+const loadfun = () => {
+    load.style.animation = "fade 0.2s linear";
+    load.style.animationDelay = "1.8s";
+    setTimeout( () => {
+        load.style.display = "none";
+    }, 2000)
+}
+
+//Labo
+const labos = [
+    {
+        number: 3,
+        link: "./labo_3/index.html",
+        color: "black"
+    },
+    {
+        number: 4,
+        link: "./labo_4/index.html",
+        color: "red"
+    },
+    {
+        number: 5,
+        link: "./labo_5/index.html",
+        color: "green"
+    }];
+
+//DOM elements initialization
+const laboTitle = document.querySelector(".labo-title");
+const laboNumber = document.querySelector(".labo-number");
+const laboText = document.querySelector(".labo-text");
+const laboLink = document.querySelector(".labo-link");
+const controlPrev = document.querySelector(".control-prev");
+const controlNext = document.querySelector(".control-next");
+
+const animateLabo = () => {
+    laboNumber.style.animation = "fadelabo 0.4s linear";
+    laboText.style.animation = "fadelabo 0.4s linear";
+
+    setTimeout(() => {
+        laboNumber.style.animation = "";
+        laboText.style.animation = "";
+    }, 400)
+}
+
+//Setup of labo
+let i = 0
+const setupLabo = () => {
+    animateLabo();
+
+    setTimeout(() => {
+        if (labos[i].number < 10) {
+            laboNumber.innerText = `0${labos[i].number.toString()}.`;
+        } else {
+            laboNumber.innerText = (labos[i].number.toString() + ".");
+        }
+        laboText.innerText = "This is the contents and open projects of labo " + labos[i].number.toString()
+        laboLink.setAttribute("href", labos[i].link);
+    }, 200)
+}
+setupLabo();
+
+//Labo controls
+const prevLabo = () => {
+    if(labos[i - 1] != null) {
+        i--
+        setupLabo();
+    }
+    else {
+        i = labos.length - 1
+        setupLabo();
+    }
+}
+
+const nextLabo = () => {
+    if(labos[i + 1] != null) {
+        i++
+        setupLabo();
+    }
+    else {
+        i = 0
+        setupLabo();
+    }
+}
+
+
+
