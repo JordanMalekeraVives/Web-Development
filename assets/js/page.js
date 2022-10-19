d729e1739f3dedb0b674f977022819a4e55f5116
JordanMalekeraVives <jordan.malekera@student.vives.be>
2022-10-19 15:34:19 +0200

    Added new labo + cleanup

--- assets/js/page.js
+++ assets/js/page.js
@@ -2,37 +2,31 @@
 const labos = [
     {
         number: 3,
-        link: "./labo_3/index.html",
         color: "black",
         projects: [{ name: "Contact assignment", link: "./labo_3/contact/index.html" }, { name: "Homepage assignment", link: "./labo_3/homepage/index.html" }, { name: "Onderwijsaanbod assignment", link: "./labo_3/opleidingsaanbod/index.html" }, { name: "Opdrachten 1 - 3", link: "./labo_3/opdrachten_1_2_3.html" }]
     },
     {
         number: 4,
-        link: "./labo_4/index.html",
         color: "#a10218",
         projects: [{ name: "Opdracht 1", link: "./labo_4/opdracht_1.html" }, { name: "Opdracht 2", link: "./labo_4/opdracht_2.html" }, { name: "Opdracht 5", link: "./labo_4/opdracht_5/index.html" }]
     },
     {
         number: 5,
-        link: "./labo_5/index.html",
         color: "#107201",
-        projects: []
+        projects: [{ name: "Opdracht 1", link: "./labo_5/opdracht_1.html" }, { name: "Opdracht 3", link: "./labo_5/opdracht_3.html" }, { name: "Opdracht 4", link: "./labo_5/opdracht_4.html" }, { name: "Opdracht 6", link: "./labo_5/opdracht_6/index.html" }, { name: "Opdracht 7", link: "./labo_5/opdracht_7.html" }]
     },
     {
         number: 6,
-        link: "./labo_6/index.html",
         color: "#034b52",
         projects: []
     },
     {
         number: 7,
-        link: "./labo_7/index.html",
         color: "#433610",
         projects: []
     },
     {
         number: 8,
-        link: "./labo_8/index.html",
         color: "#3f037c",
         projects: []
     },
@@ -50,18 +44,20 @@ const controlNext = document.querySelector(".control-next");
 const background = document.querySelector("#particles-js")
 
 //add animation to elements
-const animateLabo = () => {
-    laboNumber.style.animation = "fadelabo 0.4s linear";
-    laboText.style.animation = "fadelabo 0.4s linear";
-    laboSelect.style.animation = "fadelabo 0.4s linear";
-    background.style.animation = "fadecolor 0.4s linear";
+const animateLabo = (array) => {
+    if(array) {
+        array.forEach((element) => {
+            element.style.animation = "fadelabo 0.4s linear";
+        })
+        background.style.animation = "fadecolor 0.4s linear";
 
-    setTimeout(() => {
-        laboNumber.style.animation = "";
-        laboText.style.animation = "";
-        laboSelect.style.animation = "";
-        background.style.animation = "";
-    }, 400)
+        setTimeout(() => {
+            array.forEach((element) => {
+                element.style.animation = "";
+            })
+            background.style.animation = "";
+        }, 400)
+    }
 }
 
 //Setup of labo
@@ -69,7 +65,7 @@ let i = 0
 
 const setupLabo = (animate) => {
     if(animate == true) {
-        animateLabo()
+        animateLabo([laboNumber, laboText, laboSelect])
     }
 
     setTimeout(() => {
@@ -124,7 +120,7 @@ const displayProjects = () => {
         laboSelect.removeAttribute("onclick");
         laboSelect.style.cursor = "auto";
         displayed = true
-        animateLabo()
+        animateLabo([laboNumber, laboText, laboSelect])
         
         setTimeout(() => {
             laboText.style.display = "none";
@@ -145,7 +141,7 @@ const displayProjects = () => {
 }
 
 const removeProjects = () => {
-    animateLabo()
+    animateLabo([laboNumber, laboText, laboSelect])
     setTimeout(() => {
         laboText.style.display = "block";
         displayed = false
