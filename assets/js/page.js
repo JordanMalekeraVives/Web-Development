022a9d116c6c89bc9cf5d2af36b10016ec2656ee
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-12-04 11:48:56 +0100

    Added and completed labo 9

--- assets/js/page.js
+++ assets/js/page.js
@@ -4,37 +4,85 @@ const labos = [
         number: 3,
         color: "black",
         current: false,
-        projects: [{ name: "Contact assignment", link: "./labo_3/contact/index.html" }, { name: "Homepage assignment", link: "./labo_3/homepage/index.html" }, { name: "Onderwijsaanbod assignment", link: "./labo_3/opleidingsaanbod/index.html" }, { name: "Opdrachten 1 - 3", link: "./labo_3/opdrachten_1_2_3.html" }]
+        projects: [{name: "Contact assignment", link: "./labo_3/contact/index.html"}, {
+            name: "Homepage assignment",
+            link: "./labo_3/homepage/index.html"
+        }, {
+            name: "Onderwijsaanbod assignment",
+            link: "./labo_3/opleidingsaanbod/index.html"
+        }, {name: "Opdrachten 1 - 3", link: "./labo_3/opdrachten_1_2_3.html"}]
     },
     {
         number: 4,
         color: "#a10218",
         current: false,
-        projects: [{ name: "Opdracht 1", link: "./labo_4/opdracht_1.html" }, { name: "Opdracht 2", link: "./labo_4/opdracht_2.html" }, { name: "Opdracht 5", link: "./labo_4/opdracht_5/index.html" }]
+        projects: [{name: "Opdracht 1", link: "./labo_4/opdracht_1.html"}, {
+            name: "Opdracht 2",
+            link: "./labo_4/opdracht_2.html"
+        }, {name: "Opdracht 5", link: "./labo_4/opdracht_5/index.html"}]
     },
     {
         number: 5,
         color: "#107201",
         current: false,
-        projects: [{ name: "Opdracht 1", link: "./labo_5/opdracht_1.html" }, { name: "Opdracht 3", link: "./labo_5/opdracht_3.html" }, { name: "Opdracht 4", link: "./labo_5/opdracht_4.html" }, { name: "Opdracht 6", link: "./labo_5/opdracht_6/index.html" }, { name: "Opdracht 7", link: "./labo_5/opdracht_7.html" }]
+        projects: [{name: "Opdracht 1", link: "./labo_5/opdracht_1.html"}, {
+            name: "Opdracht 3",
+            link: "./labo_5/opdracht_3.html"
+        }, {name: "Opdracht 4", link: "./labo_5/opdracht_4.html"}, {
+            name: "Opdracht 6",
+            link: "./labo_5/opdracht_6/index.html"
+        }, {name: "Opdracht 7", link: "./labo_5/opdracht_7.html"}]
     },
     {
         number: 6,
         color: "#034b52",
         current: false,
-        projects: [{name: "Opdracht 1", link: "./labo_6/opdracht_1/index.html"}, {name: "Opdracht 3", link: "./labo_6/opdracht_3/index.html"}, {name: "Opdracht 5", link: "./labo_6/opdracht_5/index.html"}, {name: "Opdracht 6", link: "./labo_6/opdracht_6/index.html"}]
+        projects: [{name: "Opdracht 1", link: "./labo_6/opdracht_1/index.html"}, {
+            name: "Opdracht 3",
+            link: "./labo_6/opdracht_3/index.html"
+        }, {name: "Opdracht 5", link: "./labo_6/opdracht_5/index.html"}, {
+            name: "Opdracht 6",
+            link: "./labo_6/opdracht_6/index.html"
+        }]
     },
     {
         number: 7,
         color: "#433610",
         current: false,
-        projects: [{name: "Homepagina", link: "./labo_7/opdracht_homepage/index.html"}, {name: "Nature blog", link: "./labo_7/opdracht_nature_blog/index.html"}, {name: "Kalender", link: "./labo_7/opdracht_kalender/index.html"}]
+        projects: [{name: "Homepagina", link: "./labo_7/opdracht_homepage/index.html"}, {
+            name: "Nature blog",
+            link: "./labo_7/opdracht_nature_blog/index.html"
+        }, {name: "Kalender", link: "./labo_7/opdracht_kalender/index.html"}]
     },
     {
         number: 8,
         color: "#3f037c",
         current: true,
-        projects: [{name: "Opdracht 1", link: "./labo_8/opdracht_1/index.html"}, {name: "Opdracht 2", link: "./labo_8/opdracht_2/index.html"}, {name: "Opdracht 3", link: "./labo_8/opdracht_3/index.html"}, {name: "Opdracht 4", link: "./labo_8/opdracht_4/index.html"}, {name: "Opdracht 5", link: "./labo_8/opdracht_5/index.html"}, {name: "Opdracht lorem ipsum", link: "./labo_8/opdracht_lorem_ipsum/index.html"}, {name: "Opdracht lorem ipsum extra space", link: "./labo_8/opdracht_lorem_ipsum_extra_space/index.html"}, {name: "Opdracht cocktail bar", link: "./labo_8/opdracht_cocktail_bar/index.html"}]
+        projects: [{name: "Opdracht 1", link: "./labo_8/opdracht_1/index.html"}, {
+            name: "Opdracht 2",
+            link: "./labo_8/opdracht_2/index.html"
+        }, {name: "Opdracht 3", link: "./labo_8/opdracht_3/index.html"}, {
+            name: "Opdracht 4",
+            link: "./labo_8/opdracht_4/index.html"
+        }, {name: "Opdracht 5", link: "./labo_8/opdracht_5/index.html"}, {
+            name: "Opdracht lorem ipsum",
+            link: "./labo_8/opdracht_lorem_ipsum/index.html"
+        }, {
+            name: "Opdracht lorem ipsum extra space",
+            link: "./labo_8/opdracht_lorem_ipsum_extra_space/index.html"
+        }, {name: "Opdracht cocktail bar", link: "./labo_8/opdracht_cocktail_bar/index.html"}]
+    },
+    {
+        number: 8,
+        color: "#3f037c",
+        current: true,
+        projects: [{
+            name: "Opdracht positioneren",
+            link: "./labo_9/opdracht_positioneren/index.html"
+        }, {
+            name: "Opdracht airbus",
+            link: "./labo_9/opdracht_airbus/index.html"
+        }]
     },
 ];
 
@@ -52,7 +100,7 @@ const background = document.querySelector("#particles-js")
 
 //add animation to elements
 const animateLabo = (array) => {
-    if(array) {
+    if (array) {
         array.forEach((element) => {
             element.style.animation = "fadelabo 0.4s ease-in";
         })
@@ -70,9 +118,9 @@ const animateLabo = (array) => {
 //Setup of labo
 // start at labo 6
 let i = labos.length - 1;
-    
+
 const setupLabo = (animate) => {
-    if(animate == true) {
+    if (animate == true) {
         animateLabo([laboNumber, laboText, laboSelect, laboCurrent])
     }
 
@@ -86,15 +134,13 @@ const setupLabo = (animate) => {
         if (labos[i].projects.length != 0) {
             selectedProject.innerText = "Select a project";
             laboSelect.classList.remove("unavailable");
-        }
-        else {
+        } else {
             selectedProject.innerText = "Not available";
             laboSelect.classList.add("unavailable");
         }
-        if(labos[i].current == true) {
+        if (labos[i].current == true) {
             laboCurrent.style.display = "flex";
-        }
-        else {
+        } else {
             laboCurrent.style.display = "none";
         }
         background.style.backgroundColor = labos[i].color;
@@ -104,12 +150,11 @@ setupLabo();
 
 //Labo controls
 const prevLabo = () => {
-    if(labos[i - 1] != null) {
-        i--        
+    if (labos[i - 1] != null) {
+        i--
         removeProjects();
         setupLabo(true);
-    }
-    else {
+    } else {
         i = labos.length - 1
         removeProjects();
         setupLabo(true);
@@ -117,12 +162,11 @@ const prevLabo = () => {
 }
 
 const nextLabo = () => {
-    if(labos[i + 1] != null) {
+    if (labos[i + 1] != null) {
         i++
         removeProjects();
         setupLabo(true);
-    }
-    else {
+    } else {
         i = 0
         removeProjects();
         setupLabo(true);
@@ -130,27 +174,29 @@ const nextLabo = () => {
 }
 var displayed;
 const displayProjects = () => {
-    if(labos[i].projects.length != 0) {
+    if (labos[i].projects.length != 0) {
         laboSelect.removeAttribute("onclick");
         laboSelect.style.cursor = "auto";
         displayed = true
         animateLabo([laboNumber, laboText, laboSelect, laboCurrent])
-        
+
         setTimeout(() => {
             laboText.style.display = "none";
             selectedProject.style.display = "none";
             labos[i].projects.forEach((project) => {
                 const lProject = document.createElement("a")
                 lProject.innerText = project.name
-                lProject.addEventListener("click", () => {selectedProject.innerText = lProject.innerText})
+                lProject.addEventListener("click", () => {
+                    selectedProject.innerText = lProject.innerText
+                })
                 lProject.classList.add("labo-option");
                 lProject.setAttribute("href", project.link)
                 lProject.setAttribute("target", "_blank")
                 lProject.setAttribute("rel", "noopener noreferrer")
                 laboSelect.insertAdjacentElement("beforeend", lProject)
-            })  
+            })
         }, 200)
-       
+
     }
 }
 
@@ -172,9 +218,9 @@ const openLabo = () => {
 }
 
 document.addEventListener("click", (evt) => {
-    if(laboSelect != evt.target && evt.target.tagName.toLowerCase() != "a" && displayed == true && laboSelect != document.activeElement) {
+    if (laboSelect != evt.target && evt.target.tagName.toLowerCase() != "a" && displayed == true && laboSelect != document.activeElement) {
         removeProjects()
-    }  
+    }
 })
 
 
