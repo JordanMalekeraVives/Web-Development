d39f0fdb671d2cc9a22954da5a4a9356ec294071
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2023-03-19 12:23:02 +0100

    Added Labo 16

--- labo_16/opdracht_formwaarden/js/script.js
+++ labo_16/opdracht_formwaarden/js/script.js
@@ -8,6 +8,12 @@ const logger = () => {
     let conRoker = (roker) ? "Is een roker." : "Is geen roker."
     console.log(conRoker);
 
+    let taal = [...document.getElementsByName("rdbTaal").values()].filter((element) => element.checked);
+    console.log("Moedertaal is " + taal[0].value)
+
+    let buurland = document.querySelector("#sltLand").value;
+    console.log("Favoriete buurland is " + buurland)
+
     let bestelling = [...document.querySelector("#sltBestelling").selectedOptions].map((element) => element.value);
     console.log("Bestelling bestaat uit", bestelling.join(" "))
 }
