4976b387b845cb9b1a73c3c121fe03b9e2edaf15
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2023-03-19 12:13:42 +0100

    Added Labo 16

--- labo_16/opdracht_formwaarden/js/script.js
+++ labo_16/opdracht_formwaarden/js/script.js
@@ -8,6 +8,7 @@ const logger = () => {
     let conRoker = (roker) ? "Is een roker." : "Is geen roker."
     console.log(conRoker);
 
-
+    let bestelling = [...document.querySelector("#sltBestelling").selectedOptions].map((element) => element.value);
+    console.log("Bestelling bestaat uit", bestelling.join(" "))
 }
 window.addEventListener("load", setup);
